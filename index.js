4106ef1c5e989745ab35722771dca14f8f5b50d0
Benjamin Ruiz <Benjaminvruiz@gmail.com>
2022-03-01 14:27:38 -0600

    grid added, colored and divided. Working on snake

--- index.js
+++ index.js
@@ -10,22 +10,37 @@ let snake = {
 };
 
 const board = document.querySelector(".board");
-console.log(board);
+//selecting our board class from html and giving it a variable for JS to play with.
 const grid = document.createElement("table");
-grid.classList.add("snake-grid");
-board.appendChild(grid);
+//here we gave variable name grid to new element we created, and then we can add a .class and pin to board.
+grid.classList.add("snake-grid"); //we add the class so we can edit in index.css
+board.appendChild(grid); //pins the element to the board. This new elem
 
 function makeGrid() {
+  //assign variable to x and y cooridnates as well as counter.
   for (let i = 0; i < 20; i++) {
     for (let k = 0; k < 20; k++) {
       const gridCell = document.createElement("div");
       gridCell.classList.add("cell");
+      //assign ids to cells to assist with navigation and selection - need way for .css to target and make changes.
+      gridCell.setAttribute("grid-cell", [0][0]); //need to figure out how to assign index values like a grid.. For loop?
+      //will need a counter that checks when we are at the end of grid,
+      // when hit 21, increment y coordinate, and reset x and counter to 0 - do this with if()
+
       grid.appendChild(gridCell);
     }
   }
 }
 
 makeGrid();
+
+//create a function to 'draw' our snake on the grid. remember, the grid is nested arrays.
+//need to loop over our snake, and get the indexes of each body segment, and then color those sections on our grid.
+//also need to include how our snake moves forward.
+function drawSnek() {
+  /* for(let */
+}
+
 //make sure you set up how you will display the game. How do we add the board to the div on the HTML??
 //we may want to manipulate that board as well. Give it cells and our snake from above.
 //we also want our apple to appear on the board. buttons as well for control.
